f1bc4ddd55e547bdc01e6fdf5c17214cbaceb64a
achmadsyarif <19kaizen@gmail.com>
2021-09-03 09:34:39 +0700

    edit api2.dart

--- SimpelTodoApp/lib/api2.dart
+++ SimpelTodoApp/lib/api2.dart
@@ -1,1 +1,16 @@
-Buat file api
+import 'material.dart';
+class Fuitur extends StatefulWidget {
+  Fuitur({Key? key}) : super(key: key);
+
+  @override
+  _FuiturState createState() => _FuiturState();
+}
+
+class _FuiturState extends State<Fuitur> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
